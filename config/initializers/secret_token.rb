fda031259dad819a89d4e93eae3fe7f93c9cee1c
warbot <warbot.ted@gmail.com>
2015-05-25 16:38:04 +0300

    webrick with openssl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testopensll::Application.config.secret_token = 'ab4d15edb2f5edb5a971722f0881ed14be55684e5df15865a1da35fc60555ba1cec90d8474eba73337059667e5f0646ca9c7825cf9ec2ff20c6bdbeaabf349d0'
